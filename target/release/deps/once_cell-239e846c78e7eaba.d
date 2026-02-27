0784a7899c986f13b991dfd9be2f3fd3677f3dab
artwik22 <igorartwik@proton.me>
2026-02-27 18:06:01 +0100

    battery widget fix

--- target/release/deps/once_cell-239e846c78e7eaba.d
+++ target/release/deps/once_cell-239e846c78e7eaba.d
@@ -1,9 +1,9 @@
-/home/iartwik/.config/alloy/fuse/target/release/deps/once_cell-239e846c78e7eaba.d: /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs
+/home/iartwik/.config/alloy/fuse/target/release/deps/once_cell-239e846c78e7eaba.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs
 
-/home/iartwik/.config/alloy/fuse/target/release/deps/libonce_cell-239e846c78e7eaba.rlib: /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs
+/home/iartwik/.config/alloy/fuse/target/release/deps/libonce_cell-239e846c78e7eaba.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs
 
-/home/iartwik/.config/alloy/fuse/target/release/deps/libonce_cell-239e846c78e7eaba.rmeta: /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs
+/home/iartwik/.config/alloy/fuse/target/release/deps/libonce_cell-239e846c78e7eaba.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs
 
-/home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs:
-/home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs:
-/home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs:
--- target/release/deps/slab-69ca307d18d58b07.d
+++ target/release/deps/slab-69ca307d18d58b07.d
@@ -1,8 +1,8 @@
-/home/iartwik/.config/alloy/fuse/target/release/deps/slab-69ca307d18d58b07.d: /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs
+/home/iartwik/.config/alloy/fuse/target/release/deps/slab-69ca307d18d58b07.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs
 
-/home/iartwik/.config/alloy/fuse/target/release/deps/libslab-69ca307d18d58b07.rlib: /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs
+/home/iartwik/.config/alloy/fuse/target/release/deps/libslab-69ca307d18d58b07.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs
 
-/home/iartwik/.config/alloy/fuse/target/release/deps/libslab-69ca307d18d58b07.rmeta: /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs /home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs
+/home/iartwik/.config/alloy/fuse/target/release/deps/libslab-69ca307d18d58b07.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs
 
-/home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs:
-/home/iartwik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs:
